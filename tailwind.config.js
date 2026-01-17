b16dc28fc4830d82882ee190fe4198b1badd7d41
SoleScopeAI <kevin@solescope.co.uk>
2026-01-17 15:00:44 +0000

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,10 @@ export default {
         'lg': '0 10px 15px -3px rgba(255, 255, 255, 0.1), 0 4px 6px -2px rgba(255, 255, 255, 0.05)',
         'xl': '0 20px 25px -5px rgba(255, 255, 255, 0.1), 0 10px 10px -5px rgba(255, 255, 255, 0.04)',
       },
+      zIndex: {
+        '60': '60',
+        '70': '70',
+      },
     },
   },
   plugins: [
